83a37bd8e13b1ee7aac2cc1f9a25938a577112a7
pranjalanimesh <pranjalanimesh5@gmail.com>
2024-06-05 11:04:07 +0900

    coded the js ui for 3 player simulation

--- src/PhaserGame.js
+++ src/PhaserGame.js
@@ -26,13 +26,22 @@ const PhaserGame = () => {
 
         function preload() {
             this.load.image('village', 'assets/map2.png');
-            this.load.image('player', 'assets/player.png');
+            this.load.image('player1', 'assets/player.png');
+            this.load.image('player2', 'assets/player.png');
+            this.load.image('player3', 'assets/player.png');
         }
 
         function create() {
             this.add.image(640, 360, 'village');
-            // const player = this.add.image(600,300, 'player');
-            // player.setScale(0.3);
+
+            this.player1 = this.add.sprite(-20, -20, 'player1');
+            this.player1.setScale(0.3);
+
+            this.player2 = this.add.sprite(-20, -20, 'player2');
+            this.player2.setScale(0.3);
+
+            this.player3 = this.add.sprite(-20, -20, 'player3');
+            this.player3.setScale(0.3);
 
             // Connect to WebSocket server
             socket = new WebSocket('ws://localhost:6789');
@@ -45,24 +54,15 @@ const PhaserGame = () => {
                 // Update the number of villagers
                 setNumVillagers(gameState.numVillagers);
 
-                const vil = [];
-
-
-                gameState.villagers.forEach((villager) => {
-                    let village_player = this.add.image(villager.x, villager.y, 'player');
-                    village_player.setScale(0.3);
-                    vil.push(village_player);
-                    
-                });
-                setVillagers(vil);
+                this.player1.setPosition(gameState.villagers[0].x, gameState.villagers[0].y)
+                this.player2.setPosition(gameState.villagers[1].x, gameState.villagers[1].y)
+                this.player3.setPosition(gameState.villagers[2].x, gameState.villagers[2].y)
 
             };
         }
 
         function update() {
             // Game logic here
-            
-
 
         }
 
