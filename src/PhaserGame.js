0ce31a356313de5f104ddb45ebe4c0bd9445ed9c
pranjalanimesh <pranjalanimesh5@gmail.com>
2024-06-06 12:04:45 +0900

    added upscaled images and removed not in use assets

--- src/PhaserGame.js
+++ src/PhaserGame.js
@@ -25,20 +25,48 @@ const PhaserGame = () => {
 		const game = new Phaser.Game(config);
 
 		function preload() {
-			this.load.image("village", "assets/map2.png");
+			this.load.image("villageDay", "assets/map1_day_upscaled.png");
+			this.load.image("villageNight", "assets/map1_night_upscaled.png");
 			this.load.image("player", "assets/player.png");
 		}
 
 		function create() {
-			this.add.image(640, 360, "village");
+			const villageBGNight = this.add.image(640, 357, "villageDay");
+            const villageBGDay = this.add.image(640, 360, "villageNight");
+            villageBGDay.setScale(0.25);
+            villageBGNight.setScale(0.25);
+            let x = false;
+
+            setInterval(() => {
+                console.log("changing background");
+                console.log(x)
+
+                if (x) {
+                    villageBGDay.setAlpha(1);
+                    villageBGNight.setAlpha(0);
+                    console.log("day");
+                    console.log("to night")
+                }
+                else {
+                    villageBGDay.setAlpha(0);
+                    villageBGNight.setAlpha(1);
+                    console.log("night");
+                    console.log("to day")
+                }
+
+                x= !x;
+
+            }, 25000);
 
 			// Connect to WebSocket server
 			socket = new WebSocket("ws://localhost:6789");
 
 			// Listen for messages from the server
 			socket.onmessage = (event) => {
+                
+                // Parse the JSON message
 				const gameState = JSON.parse(event.data);
-				console.log(gameState);
+				// console.log(gameState);
 
 				// If villagers are not created yet, create them
 				if (villagerSprites.length === 0) {
@@ -61,6 +89,7 @@ const PhaserGame = () => {
 
 		function update() {
 			// Game logic here
+
 		}
 
 		return () => {
@@ -72,8 +101,9 @@ const PhaserGame = () => {
 		};
 	}, []);
 
-	console.log("villagers");
-	console.log(villagers);
+	// console.log("villagers");
+	// console.log(villagers);
+    // console.log(day);
 	return <div ref={gameContainer} />;
 };
 
